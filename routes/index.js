2de337c4e89ed32fe8dc3aa79830350100d352cf
unknown <ashishuiux@gmail.com>
2018-11-09 19:42:58 -0800

    replace sequelize with mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,24 @@
-const router = require('express').Router();
+// const router = require("express").Router();
 
-const profile = require("../controllers/profile");
-const quiz = require("../controllers/quizes");
+// // const profile = require("../controllers/profile");
+// // const quiz = require("../controllers/quizes");
 
-/*
- * routes with 'temp-done' status, need data from the front-end to be complete. 
- */
+// /*
+//  * routes with 'temp-done' status, need data from the front-end to be complete.
+//  */
 
-module.exports = router
-  // profile routes
-  .get("/all-users", profile.all)
-  .get("/user-check", profile.findUser)
-  .post("/create-user", profile.createUser)
-  // quiz routes
-  .post("/create-question", quiz.createQuestion)
-  .get("/all-quizzes", quiz.findAllQuizzes)
-  .post("/edit-question/:title/:qNum", quiz.editQuestion)
-  .get("/find-question/:title/:qNum", quiz.findOneQuestion)
-  // route for editing quizzes
-  .get("/find-one-quiz/:title", quiz.findOneQuiz)
-  // route for deleting questions
-  .post("/delete-question/:title/:qNum", quiz.deleteQuestion)
-  .post("/delete-quiz/:title", quiz.deleteQuiz);
+// module.exports = router
+//   // profile routes
+//   .get("/all-users", profile.all)
+//   .get("/user-check", profile.findUser)
+//   .post("/create-user", profile.createUser)
+//   // quiz routes
+//   .post("/create-question", quiz.createQuestion)
+//   .get("/all-quizzes", quiz.findAllQuizzes)
+//   .post("/edit-question/:title/:qNum", quiz.editQuestion)
+//   .get("/find-question/:title/:qNum", quiz.findOneQuestion)
+//   // route for editing quizzes
+//   .get("/find-one-quiz/:title", quiz.findOneQuiz)
+//   // route for deleting questions
+//   .post("/delete-question/:title/:qNum", quiz.deleteQuestion)
+//   .post("/delete-quiz/:title", quiz.deleteQuiz);
--- server.js
+++ server.js
@@ -1,38 +1,32 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const routes = require("./routes");
+// const routes = require("./routes");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
-const db = require("./models");
-const AWS = require('aws-sdk');
+const mongoose = require("mongoose");
+const AWS = require("aws-sdk");
 
 AWS.config.region = process.env.REGION;
-
-// Serve static files from the React app (New website)
-app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// add routes
-app.use("/",routes);
-console.log(`Node env is: ${process.env.NODE_ENV}`);
-console.log(`Databse name is: ${process.env.DB_STAGING_NAME}`);
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  if(process.env.NODE_ENV === "production"){
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  }else{
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+// Mongoose settings
+mongoose.Promise = global.Promise;
+// connect to Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/tutortimes",
+  {
+    useNewUrlParser: true
   }
-});
+);
+
+// add routes
+// app.use("/", routes);
 
 // listen on PORT
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log(`🌎 ==> Server now on port ${PORT}!`);
-  });
+app.listen(PORT, function() {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
