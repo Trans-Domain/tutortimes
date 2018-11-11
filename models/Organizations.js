d17337a4d06aaa6b92c87530d8cbfddaab518bad
unknown <ashishuiux@gmail.com>
2018-11-10 18:18:05 -0800

    add Organizations model

---
+++ models/Organizations.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema();
+
+const OrganizationSchema = new Schema({
+  name: { type: String, required: true },
+  address: { type: String, required: true },
+  dateJoined: { type: Date, required: true },
+  billingDate: { type: Date },
+  tutors: [
+    {
+      tutorId: { type: ObjectId, ref: "Tutor" }
+    }
+  ],
+  students: [
+    {
+      studentId: { type: ObjectId, ref: "Tutor" }
+    }
+  ]
+});
+
+const Organizations = mongoose.model("Organizations", OrganizationSchema);
+
+module.exports = Organizations;
