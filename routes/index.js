a6988a9dd07cf3ef78efd04894d897b0d3233562
unknown <ashishuiux@gmail.com>
2018-11-14 17:00:27 -0800

    add comments to segment routes

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ const router = require("express").Router();
 const user = require("../controllers/users");
 
 module.exports = router
-  // Users
+  // Organization
   .post("/create-organization", user.organization.create)
   .get("/find-organization/:name", user.organization.find)
   .get("/find-all-organizations", user.organization.findAll)
   .put("/update-organization", user.organization.update)
   .delete("/delete-organization", user.organization.delete)
+  // Tutor
   .post("/create-tutor", user.tutor.create)
   .post("/create-student", user.student.create);
