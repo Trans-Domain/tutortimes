5e7560238c50204d23be1f48cd9e68f45411a2b8
unknown <ashishuiux@gmail.com>
2018-11-14 17:30:54 -0800

    add controller function to find all tutors

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,6 @@ module.exports = router
   .delete("/delete-organization", user.organization.delete)
   // Tutor
   .post("/create-tutor", user.tutor.create)
+  .get("/find-all-tutors/:organization", user.tutor.findAll)
+  // Student
   .post("/create-student", user.student.create);
