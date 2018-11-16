259c1018d8885de1c2857826fffab7f9a650e1ee
unknown <ashishuiux@gmail.com>
2018-11-15 20:05:40 -0800

    write test for get tutorInfoUpdates

--- helpers/getTutorInfoUpdates/getTutorInfoUpdates.js
+++ helpers/getTutorInfoUpdates/getTutorInfoUpdates.js
@@ -7,10 +7,13 @@ const getTutorInfoUpdates = tutorObj => {
   // get array of keys and values
   let keys = Object.keys(tutorObj).map(x => `tutors.$.${x}`);
   let values = Object.values(tutorObj);
-  // modify keys and make them strings
-  return "hi";
-  // re-attach values
+  // loop through elements and create new object
+  let resultObj = {};
+  for (let i = 0; i < keys.length; i++) {
+    resultObj[keys[i]] = values[i];
+  }
   // return object
+  return resultObj;
 };
 
 export default getTutorInfoUpdates;
--- helpers/getTutorInfoUpdates/getTutorInfoUpdates.test.js
+++ helpers/getTutorInfoUpdates/getTutorInfoUpdates.test.js
@@ -1,7 +1,21 @@
-import { getTutorInfoUpdates } from "./getTutorInfoUpdates";
+import getTutorInfoUpdates from "./getTutorInfoUpdates";
 
 describe("get tutor info updates", () => {
-  test("test for hi", () => {
-    expect("hi").toBe("hi");
+  test("return bio with modified object", () => {
+    expect(getTutorInfoUpdates({ bio: "I like studying" })).toEqual({
+      "tutors.$.bio": "I like studying"
+    });
+  });
+
+  test("return bio and phone modified object", () => {
+    expect(
+      getTutorInfoUpdates({
+        bio: "I like studying",
+        phone: "123-456-7890"
+      })
+    ).toEqual({
+      "tutors.$.bio": "I like studying",
+      "tutors.$.phone": "123-456-7890"
+    });
   });
 });
