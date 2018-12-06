177dd0f2f9ef941a99e626bf1f1f74fa70b030f1
unknown <ashishuiux@gmail.com>
2018-12-05 16:59:23 -0800

    add check for 1 assertion

--- helpers/passwordHashing/passwordHashing.test.js
+++ helpers/passwordHashing/passwordHashing.test.js
@@ -2,6 +2,7 @@ import hashPassword from "./passwordHashing";
 
 describe("Make sure password gets hashed", () => {
   test("regular password does not equal hashed password", () => {
+    expect.assertions(1);
     return hashPassword({
       fullname: "John Doe",
       password: "password"
