28b9e26acbfdff547f62618c83f75b3ccb4c1c86
unknown <ashishuiux@gmail.com>
2018-12-05 16:52:28 -0800

    refactor hashPassword function

--- helpers/passwordHashing/passwordHashing.js
+++ helpers/passwordHashing/passwordHashing.js
@@ -2,17 +2,15 @@ import bcrypt from "bcrypt";
 const saltRounds = 10;
 
 const hashPassword = newUser => {
-  var promise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     bcrypt
       .hash(newUser.password, saltRounds)
-      .then(async hash => {
+      .then(hash => {
         newUser.password = hash;
         resolve(newUser);
       })
       .catch(err => reject(err));
   });
-
-  return promise;
 };
 
 export default hashPassword;
